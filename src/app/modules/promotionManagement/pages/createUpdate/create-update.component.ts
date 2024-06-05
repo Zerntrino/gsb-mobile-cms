4960abb94d06d6d77b7d022bb1ac97382c64ba0c
Suphat W <suphat.programmer@gmail.com>
2024-06-05 18:24:49 +0700

    🧩 promotion create update api

--- src/app/modules/promotionManagement/pages/createUpdate/create-update.component.ts
+++ src/app/modules/promotionManagement/pages/createUpdate/create-update.component.ts
@@ -11,6 +11,18 @@ import { AdService } from 'src/app/core/services/ad.service';
 import dayjs from 'dayjs';
 import { AngularEditorConfig } from '@kolkov/angular-editor';
 import { UtilsService } from 'src/app/core/services/utils.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { PromotionService } from 'src/app/core/services/promotion.service';
+import { ToastService } from 'src/app/core/services/toast.service';
+import { CategoryService } from 'src/app/core/services/category.service';
+import { Category } from 'src/app/core/models/category.model';
+import { PartnerService } from 'src/app/core/services/partner.service';
+import { Partner } from 'src/app/core/models/partner.model';
+import { ParameterService } from 'src/app/core/services/parameter.service';
+import { MCC } from 'src/app/core/models/parameter.model';
+import { CardService } from 'src/app/core/services/card.service';
+import { Card } from 'src/app/core/models/card.model';
+import { Promotion } from 'src/app/core/models/promotion.model';
 
 @Component({
   selector: 'app-promotion-management-create-update',
@@ -24,42 +36,80 @@ export class CreateUpdateComponent implements OnInit {
     { title: 'สร้างโปรโมชัน', to: '' },
   ];
 
-  category: Select2Value = '';
-  categoryOption: Select2Option[] = [
-    { value: 'ชอปปิง', label: 'ชอปปิง' },
-    { value: 'ร้านอาหาร', label: 'ร้านอาหาร' },
-    { value: 'ท่องเที่ยว', label: 'ท่องเที่ยว' },
-  ];
+  submitForm = new FormGroup({
+    categorId: new FormControl(0),
+    isActive: new FormControl(true),
+    name: new FormControl('', [Validators.required]),
+    description: new FormControl(''),
+    aboutIt: new FormControl<string[]>([]),
+    startDate: new FormControl('', []),
+    endDate: new FormControl('', []),
+    isNotification: new FormControl(true),
+    typeId: new FormControl(0),
+    shopId: new FormControl(0),
+    mccCode: new FormControl<string[]>([]),
+    generateType: new FormControl(0),
+    prefixCode: new FormControl(''),
+    importCode: new FormControl<string[]>([]),
+    limit: new FormControl(0),
+    limitPerMonth: new FormControl(0),
+    limitPerCardMonth: new FormControl(0),
+    limitPerCardDay: new FormControl(0),
+    cardId: new FormControl<number[]>([]),
+    coverImageUrl: new FormControl(''),
+    imageUrl: new FormControl<string[]>([]),
+  });
+
+  coverImageBase64 = '';
+  imageBase64: string[] = [];
+
+  categoryOption: Select2Option[] = [];
   tags: string[] = ['ชอปปิง', 'ร้านอาหาร', 'ท่องเที่ยว'];
 
   htmlContent = '';
   editorConfig = this.utilsService.editorConfig;
 
-  dt1 = '';
-  dt2 = '';
-
   promotionType: Select2Value = '';
   promotionTypeOption: Select2Option[] = [
     {
-      value: 'โปรโมชันลงทะเบียนรับสิทธิ์',
+      value: 1,
       label: 'โปรโมชันลงทะเบียนรับสิทธิ์',
+    },
+    {
+      value: 2,
+      label: 'โปรโมชั่นไม่ลงทะเบียนรับสิทธิ์',
+    },
+    {
+      value: 3,
+      label: 'โปรโมชั่นลงทะเบียนรับสิทธิ์',
+    },
+    {
+      value: 4,
+      label: 'โปรโมชั่นลงทะเบียนรับสิทธิ์ (แสดงโค้ด)',
     },
   ];
 
-  brand: Select2Value = '';
-  brandOption: Select2Option[] = [{ value: 'SF', label: 'SF' }];
-
-  mccs: string[] = [];
+  shopOption: Select2Option[] = [];
+  mccOption: Select2Option[] = [];
   codeType: Select2Value = '';
   codeTypeOption: Select2Option[] = [
-    { value: 'Generate code', label: 'Generate code' },
+    { value: 1, label: 'Generate code' },
+    { value: 2, label: 'Import code' },
   ];
+
+  cards: Card[] = [];
+  cardIds: number[] = [];
 
   constructor(
     private router: Router,
     activatedRoute: ActivatedRoute,
-    private adService: AdService,
-    private utilsService: UtilsService
+    private promotionService: PromotionService,
+    private categoryService: CategoryService,
+    private partnerService: PartnerService,
+    private parameterService: ParameterService,
+    private cardService: CardService,
+    private utilsService: UtilsService,
+    private toastService: ToastService
   ) {
     this.id = activatedRoute.snapshot.params['id'];
     this.navItems[1].title =
@@ -67,21 +117,112 @@ export class CreateUpdateComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    // this.editor = new Editor();
-
     this.fetch();
+    this.fetchCategory();
+    this.fetchShop();
+    this.fetchMcc();
+    this.fetchCard();
   }
 
   fetch(): void {
-    // this.adService.getList(params).subscribe(
-    //   (response) => {
-    //     console.log(response.data);
-    //     this.list = response.data as Ad[];
-    //   },
-    //   (error) => {
-    //     console.log(error);
-    //   }
-    // );
+    if (this.id != 'create') {
+      this.promotionService.get(this.id).subscribe(
+        (response) => {
+          const res = response.data as Promotion;
+          console.log(res);
+          this.submitForm.setValue({
+            categorId: res.category_id,
+            isActive: res.is_active,
+            name: res.name,
+            description: res.description,
+            aboutIt: res.about_it,
+            startDate: res.start_date,
+            endDate: res.end_date,
+            isNotification: res.is_notification,
+            typeId: res.type_id,
+            shopId: res.shop_id,
+            mccCode: res.mcc_code,
+            generateType: res.generate_type,
+            prefixCode: res.prefix_code,
+            importCode: res.import_code || [],
+            limit: res.limit,
+            limitPerMonth: res.limit_per_month,
+            limitPerCardMonth: res.limit_per_card_month,
+            limitPerCardDay: res.limit_per_card_day,
+            cardId: res.card_id,
+            coverImageUrl: res.cover_url,
+            imageUrl: res.image_url,
+          });
+
+          this.imageBase64 = res.imageUrl;
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    }
+  }
+
+  fetchCategory(): void {
+    let params = new HttpParams().append('page', 1).append('pageSize', 100);
+    this.categoryService.getList(params).subscribe(
+      (response) => {
+        const categories = response.data as Category[];
+        this.categoryOption = this.categoryOption.concat(
+          categories.map((c) => {
+            return { value: c.id, label: c.name } as Select2Option;
+          })
+        );
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  fetchShop(): void {
+    let params = new HttpParams().append('page', 1).append('pageSize', 100);
+    this.partnerService.getList(params).subscribe(
+      (response) => {
+        const partners = response.data as Partner[];
+        this.shopOption = partners.map((c) => {
+          return { value: c.id, label: c.name } as Select2Option;
+        });
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  fetchMcc(): void {
+    this.parameterService.getMCCList().subscribe(
+      (response) => {
+        const mccs = response.data as MCC[];
+        this.mccOption = mccs.map((item) => {
+          return {
+            value: item.code,
+            label: `${item.code} : ${item.name}`,
+          } as Select2Option;
+        });
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  fetchCard(): void {
+    let params = new HttpParams().append('page', 1).append('pageSize', 100);
+    this.cardService.getList(params).subscribe(
+      (response) => {
+        this.cards = response.data as Card[];
+        this.cardIds = this.cards.map((c) => c.id);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 
   dateFormat(d: string): string {
@@ -90,29 +231,123 @@ export class CreateUpdateComponent implements OnInit {
   }
 
   categoryChange(e: Select2UpdateEvent): void {
-    if (this.category != e.value) {
-      this.category = e.value;
+    if (this.submitForm.get('categorId')?.value != e.value) {
+      this.submitForm.get('categorId')?.setValue(e.value as number);
     }
   }
   promotionTypeChange(e: Select2UpdateEvent): void {
-    if (this.promotionType != e.value) {
-      this.promotionType = e.value;
+    if (this.submitForm.get('typeId')?.value != e.value) {
+      this.submitForm.get('typeId')?.setValue(e.value as number);
     }
   }
   tagChange(e: string[]): void {
-    this.tags = e;
+    this.submitForm.get('aboutIt')?.setValue(e);
   }
-  brandChange(e: Select2UpdateEvent): void {
-    if (this.brand != e.value) {
-      this.brand = e.value;
+  shopChange(e: Select2UpdateEvent): void {
+    if (this.submitForm.get('shopId')?.value != e.value) {
+      this.submitForm.get('shopId')?.setValue(e.value as number);
     }
   }
-  mccChange(e: string[]): void {
-    this.mccs = e;
+  mccChange(e: Select2UpdateEvent): void {
+    if (this.submitForm.get('mccCode')?.value != e.value) {
+      this.submitForm.get('mccCode')?.setValue((e.value as string[]) || []);
+    }
   }
-  codeTypeChange(e: Select2UpdateEvent): void {
-    if (this.codeType != e.value) {
-      this.codeType = e.value;
+  generateTypeChange(e: Select2UpdateEvent): void {
+    if (this.submitForm.get('generateType')?.value != e.value) {
+      this.submitForm.get('generateType')?.setValue(e.value as number);
+    }
+  }
+
+  updateCard(event: boolean, id: number) {
+    if (event)
+      this.submitForm
+        .get('cardId')
+        ?.setValue(this.submitForm.get('cardId')?.value?.concat([id]) || []);
+    else {
+      const filter = this.submitForm
+        .get('cardId')
+        ?.value?.filter((c) => c != id);
+
+      this.submitForm.get('cardId')?.setValue(filter || []);
+    }
+  }
+
+  inputFileChange(event: Event): void {
+    const files = (event.target as HTMLInputElement).files;
+    const file = files?.item(0);
+    if (file) {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        this.coverImageBase64 = reader.result?.toString() || '';
+      };
+    }
+  }
+
+  inputFile2Change(event: Event): void {
+    const files = (event.target as HTMLInputElement).files;
+    const file = files?.item(0);
+    if (file) {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        this.imageBase64.push(reader.result?.toString() || '');
+      };
+    }
+  }
+
+  removeImg(index: number): void {
+    this.imageBase64 = this.imageBase64.filter((item, i) => i != index);
+  }
+
+  async onSubmit() {
+    if (this.coverImageBase64) {
+      const upload = await this.promotionService
+        .upload({
+          imageBase64: this.imageBase64,
+        })
+        .toPromise();
+      this.submitForm.get('coverImageUrl')?.setValue(upload?.data || '');
+    }
+
+    if (this.imageBase64.length) {
+      await this.imageBase64.forEach(async (img, i) => {
+        if (!img.startsWith('http')) {
+          const upload = await this.promotionService
+            .upload({
+              imageBase64: img,
+            })
+            .toPromise();
+
+          this.imageBase64[i] = upload?.data || '';
+        }
+      });
+      this.submitForm.get('imageUrl')?.setValue(this.imageBase64);
+    }
+
+    if (this.id == 'create') {
+      this.promotionService.create(this.submitForm.getRawValue()).subscribe(
+        (response) => {
+          this.router.navigate(['/promotion-management']);
+        },
+        (error) => {
+          console.log(error);
+          this.toastService.add('error', error);
+        }
+      );
+    } else {
+      this.promotionService
+        .update(parseInt(this.id), this.submitForm.getRawValue())
+        .subscribe(
+          (response) => {
+            this.router.navigate(['/promotion-management']);
+          },
+          (error) => {
+            console.log(error);
+            this.toastService.add('error', error);
+          }
+        );
     }
   }
 }
