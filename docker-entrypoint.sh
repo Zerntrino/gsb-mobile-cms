bd3574edbff43da45f864457db05a2eaa15e5ebf
Suphat W <suphat.programmer@gmail.com>
2025-05-30 16:31:00 +0700

    no message

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,8 +4,6 @@ cd /app
 # Substitute environment variables in the Nginx template and save as the final config
 envsubst '$API_BACKEND' < nginx.conf > nginx.default.conf
 
-envsubst '$OAUTH2_ISSUER' < src/environments/environment.prod.ts > src/environments/environment.prod.ts
-envsubst '$OAUTH2_CLIENT_ID' < src/environments/environment.prod.ts > src/environments/environment.prod.ts
-envsubst '$OAUTH2_REDIRECT_URI' < src/environments/environment.prod.ts > src/environments/environment.prod.ts
+envsubst '$OAUTH2_ISSUER $OAUTH2_CLIENT_ID $OAUTH2_REDIRECT_URI' < src/environments/environment-template.prod.ts > src/environments/environment.prod.ts
 
 exec "$@"
