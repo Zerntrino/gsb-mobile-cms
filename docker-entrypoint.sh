2455f3173c284e45af83edac9414dd99a53d651b
Suphat W. <suphat.programmer@gmail.com>
2025-09-18 13:05:37 +0700

    ♻️ template

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@ cd /app
 # Substitute environment variables in the Nginx template and save as the final config
 envsubst '$API_BACKEND' < nginx.conf > nginx.default.conf
 
-envsubst '$APPLE_APP_ID' < .well-known/apple-app-site-association > .well-known/apple-app-site-association
+envsubst '$APPLE_APP_ID' < .well-known/apple-app-site-association-template > .well-known/apple-app-site-association
 
 envsubst '$OAUTH2_ISSUER $OAUTH2_CLIENT_ID $OAUTH2_REDIRECT_URI $XAPPID' < src/environments/environment-template.prod.ts > src/environments/environment.prod.ts
 
